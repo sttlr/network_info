d033e49783d439b596463d885d75cca5503acf3c
Max K <40246850+sttlr@users.noreply.github.com>
2024-01-13 17:26:59 +0200

    Implemented Organisation search (#1)

--- db/model.py
+++ db/model.py
@@ -16,18 +16,45 @@ class Block(Base):
     inetnum = Column(postgresql.CIDR, nullable=False, index=True)
     netname = Column(String, nullable=True, index=True)
     description = Column(String)
-    country = Column(String, index=True)
-    maintained_by = Column(String, index=True)
-    created = Column(DateTime, index=True)
-    last_modified = Column(DateTime, index=True)
     source = Column(String, index=True)
-    status = Column(String, index=True)
 
     __table_args__ = (
         Index('ix_block_description', func.to_tsvector(literal_column("'english'"), description), postgresql_using="gin"), )
 
     def __str__(self):
-        return f'inetnum: {self.inetnum}, netname: {self.netname}, desc: {self.description}, status: {self.status}, country: {self.country}, maintained: {self.maintained_by}, created: {self.created}, updated: {self.last_modified}, source: {self.source}'
+        return f'inetnum: {self.inetnum}, netname: {self.netname}, description: {self.description}, source: {self.source}'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Organisation(Base):
+    __tablename__ = 'organisation'
+    id = Column(Integer, primary_key=True)
+    organisation = Column(String, nullable=False, index=True)
+    orgname = Column(String, nullable=True, index=True)
+    source = Column(String, index=True)
+
+    def __str__(self):
+        return f'organisation: {self.organisation}, orgname: {self.orgname}, source: {self.source}'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class ASN(Base):
+    __tablename__ = 'asn'
+    id = Column(Integer, primary_key=True)
+    autnum = Column(String, nullable=False, index=True)
+    asname = Column(String, nullable=True, index=True)
+    description = Column(String)
+    source = Column(String, index=True)
+
+    __table_args__ = (
+        Index('ix_asn_description', func.to_tsvector(literal_column("'english'"), description), postgresql_using="gin"), )
+
+    def __str__(self):
+        return f'autnum: {self.autnum}, asname: {self.asname}, description: {self.description}, source: {self.source}'
 
     def __repr__(self):
         return self.__str__()
--- download_dumps.sh
+++ download_dumps.sh
@@ -6,7 +6,7 @@ mkdir -p $DOWNLOAD_DIR
 function download {
   name=$(echo $1 |awk -F "/" '{print $NF}')
   echo "Downloading $name..."
-  wget -O "$DOWNLOAD_DIR/$name" "$1"
+  wget -q -O "$DOWNLOAD_DIR/$name" "$1" &
 }
 
 download "https://ftp.afrinic.net/pub/dbase/afrinic.db.gz"
@@ -14,9 +14,21 @@ download "https://ftp.afrinic.net/pub/dbase/afrinic.db.gz"
 download "https://ftp.apnic.net/pub/apnic/whois/apnic.db.inetnum.gz"
 download "https://ftp.apnic.net/pub/apnic/whois/apnic.db.inet6num.gz"
 
-download "https://ftp.arin.net/pub/rr/arin.db.gz"
+download "https://ftp.apnic.net/apnic/whois/apnic.db.organisation.gz"
 
-download "https://ftp.lacnic.net/lacnic/dbase/lacnic.db.gz"
+download "https://ftp.apnic.net/apnic/whois/apnic.db.aut-num.gz"
+
+# it is a 'route' db
+# download "https://ftp.arin.net/pub/rr/arin.db.gz"
+
+# it doesn't contain any tangible to org info
+# download "https://ftp.lacnic.net/lacnic/dbase/lacnic.db.gz" 
 
 download "https://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz"
 download "https://ftp.ripe.net/ripe/dbase/split/ripe.db.inet6num.gz"
+
+download "https://ftp.ripe.net/ripe/dbase/split/ripe.db.organisation.gz"
+
+download "https://ftp.ripe.net/ripe/dbase/split/ripe.db.aut-num.gz"
+
+wait
